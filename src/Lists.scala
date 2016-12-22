918bc51d3cc39a1353140c20bbaf00a651a8c85a
Kashif Nazir <knazir@stanford.edu>
2016-12-22 18:14:38 +0300

    Completed through problem 8

--- src/Lists.scala
+++ src/Lists.scala
@@ -1,11 +1,58 @@
+import scala.util.Try
+import scala.util.control.Breaks._
+
 object Lists {
+
+  /* List helper functions */
+
+  def getKValue(list: List[Any]) : Int = {
+    println("Enter value for k:")
+    var k: Try[Int] = null
+    breakable {
+      while (true) {
+        k = Try(scala.io.StdIn.readLine().toInt)
+        if (k.get >= 0 && k.get < list.length) break
+        println(s"Please enter a valid integer between 0 and ${list.length}")
+      }
+    }
+    k.get
+  }
+
+  /* Problems */
 
   def P1(list: List[Any]) : Any = {
     list.last
   }
 
   def P2(list: List[Any]) : Any = {
-
+    list.init.last
   }
 
+  def P3(list: List[Any]) : Any = {
+    val k = getKValue(list)
+    list(k)
+  }
+
+  def P4(list: List[Any]) : Int = {
+    list.length
+  }
+
+  def P5(list: List[Any]) : List[Any] = {
+    list.reverse
+  }
+
+  def P6(list: List[Any]) : Boolean = {
+    list == list.reverse
+  }
+
+  // TODO: Add capabilities for inputting nested list structures
+  def P7(list: List[Any]) : List[Any] = list flatMap {
+    case listElement: List[_] => P7(listElement)
+    case flatElement => List(flatElement)
+  }
+
+  def P8(list: List[Any]): Any = list.foldLeft(List[Any]()) {
+    case (l, e) if l.isEmpty || l.last != e => l:::List(e)
+    case (l, e) => l
+  }
 }
--- src/Main.scala
+++ src/Main.scala
@@ -12,23 +12,24 @@ object Main {
 
   val ProblemTypes = List((1, "Lists"), (2, "Arithmetic"), (3, "Binary Trees"), (4, "Multiway Trees"), (5, "Graphs"),
     (6, "Miscellaneous"))
-  val ListProblems = List((1, "Last Element"), (2, "Penultimate Element"))
+  val ListProblems = List((1, "Last Element"), (2, "Penultimate Element"), (3, "K-th Element"), (4, "List Size"),
+    (5, "Reverse List"), (6, "Is List Palindrome"), (7, "Eliminate Consecutive Duplicates"))
 
 
   /* Choosing and running problems */
 
-  def printOptions(options: List[(Int, String)]): Unit = {
+  def printOptions(options: List[(Int, String)]) : Unit = {
     options.foreach(q => println(s"${q._1} - ${q._2}"))
   }
 
-  def showGreeting(): Unit = {
+  def showGreeting() : Unit = {
     println("Welcome to 99 Scala Problems. Please choose a category:")
     printOptions(ProblemTypes)
   }
 
-  def getUserChoice(options: List[(Int, String)]): Int = {
+  def getUserChoice(options: List[(Int, String)]) : Int = {
     println(s"Choose a problem ($Sentinel to quit):")
-    val firstQuestionNumber = options(0)._1
+    val firstQuestionNumber = options.head._1
     val lastQuestionNumber = firstQuestionNumber + options.length
     var choice: Try[Int] = null
 
@@ -38,13 +39,13 @@ object Main {
         if (input == "") return Sentinel
         choice = Try(input.toInt)
         if (choice.isSuccess && choice.get >= firstQuestionNumber && choice.get <= lastQuestionNumber) break
-        else println(s"Please enter a valid numbet between $firstQuestionNumber and $lastQuestionNumber")
+        println(s"Please enter a valid numbet between $firstQuestionNumber and $lastQuestionNumber")
       }
     }
     choice.get
   }
 
-  def runProblem(className: String, choice: Int, data: Any): Any = {
+  def runProblem(className: String, choice: Int, data: Any) : Any = {
     val m = ru.runtimeMirror(getClass.getClassLoader)
     val module = m.staticModule(className.dropRight(1))
     val im = m.reflectModule(module)
@@ -56,7 +57,7 @@ object Main {
 
   /* List problems */
 
-  def toInferredType(element: String): Any = {
+  def toInferredType(element: String) : Any = {
     val integer = Try(element.toInt)
     if (integer.isSuccess) return integer.get
 
@@ -69,17 +70,18 @@ object Main {
     element
   }
 
-  def createListFromInput(): List[Any] = {
+  def createListFromInput() : List[Any] = {
     println("Enter comma-separated list. Press return when finished.")
     scala.io.StdIn.readLine().split(",").map(_.trim).toList.map(toInferredType)
   }
 
-  def showListsProblems(): Unit = {
+  def showListsProblems() : Unit = {
     println("Pick a list problem:")
     printOptions(ListProblems)
 
     val choice = getUserChoice(ListProblems)
     val list = createListFromInput()
+    if (list.isEmpty) throw new IllegalArgumentException("List cannot be empty")
     println(s"Created ${list.toString()}")
 
     println(s"Result: ${runProblem(Lists.getClass.getName, choice, list)}")
@@ -88,7 +90,7 @@ object Main {
 
   /* Main function */
 
-  def main(args: Array[String]): Unit = {
+  def main(args: Array[String]) : Unit = {
     showGreeting()
     breakable {
       val choice = getUserChoice(ProblemTypes)
